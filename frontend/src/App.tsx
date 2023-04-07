df77a8ab50531129cc08a6cae1bb151791420206
LeandroMarcelo <lmarcelos2019@gmail.com>
2023-04-07 19:44:56 -0400

    feat: added sign in page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
+import SignIn from "./pages/SignIn"
+
 function App() {
   return (
     <>
-      <div className="h-screen w-full ">
-        <div className="w-full h-full flex justify-center items-center">
-          GET STARTED
-        </div>
-      </div>
+      <SignIn />
     </>
   )
 }
